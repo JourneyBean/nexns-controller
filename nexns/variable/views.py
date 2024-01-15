d1455bae5caa856b37c05c2c48fdfd1a8dd51b0b
Johnson Liu <johnson@awaworks.net>
2024-01-15 13:46:29 +0800

    add action apply to VariableView

--- nexns/variable/views.py
+++ nexns/variable/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, response, decorators
 
 from .models import Variable
 from .serializers import VariableSerializer
+from .lib import update_user_variables
 
 
 class VariableView(viewsets.ModelViewSet):
@@ -18,3 +19,11 @@ class VariableView(viewsets.ModelViewSet):
             queryset = self.queryset.filter(user=user)
 
         return queryset
+    
+    @decorators.action(detail=False, methods=['post'])
+    def apply(self, request):
+        user = self.request.query_params.get('user', None)
+        update_user_variables(user)
+        return response.Response(
+            VariableSerializer(Variable.objects.filter(user=user), many=True).data
+        )
