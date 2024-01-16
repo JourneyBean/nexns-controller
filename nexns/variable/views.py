d3c80741cecffb796a3ecfc18dcb6e3aa3f18e68
Johnson Liu <johnson@awaworks.net>
2024-01-16 23:34:44 +0800

    add bulk update api for variable view

--- nexns/variable/views.py
+++ nexns/variable/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets, response, decorators
 from .models import Variable
 from .serializers import VariableSerializer
 from .lib import update_user_variables
+from nexns.name.lib.bulk_update import bulk_update
 
 
 class VariableView(viewsets.ModelViewSet):
@@ -27,3 +28,12 @@ class VariableView(viewsets.ModelViewSet):
         return response.Response(
             VariableSerializer(Variable.objects.filter(user=user), many=True).data
         )
+
+    @decorators.action(detail=False, methods=['PUT'])
+    def bulk(self, request):
+        queryset = self.get_queryset()
+        bulk_update(queryset, request.data, self.serializer_class)
+
+        serializer = self.serializer_class(self.get_queryset(), many=True)
+        return response.Response(serializer.data)
+    
