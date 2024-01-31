1a821e19a01f0b83fe3171452f8e30eb618f849f
Johnson Liu <johnson@awaworks.net>
2024-02-01 00:47:42 +0800

    set login redirect url to home

--- main/settings.py
+++ main/settings.py
@@ -144,6 +144,8 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
+LOGIN_REDIRECT_URL = '/'
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
